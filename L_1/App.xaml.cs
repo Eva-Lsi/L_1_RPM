a2cc2fb5184f86d5762b2a05466d4cd136b105fa
Eva-Lsi <lizenoc1199@gmail.com>
2026-03-20 04:40:32 +0700

    Revert "1/2"

--- L_1/App.xaml.cs
+++ L_1/App.xaml.cs
@@ -1,9 +1,14 @@
-﻿using System.Windows;
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
 
-namespace FactoryMethod
+namespace L_1
 {
-  
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
     public partial class App : Application
     {
     }
+
 }
--- L_1/MainWindow.xaml.cs
+++ L_1/MainWindow.xaml.cs
@@ -1,61 +1,22 @@
-﻿using System.Windows;
+﻿using System.Text;
+using System.Windows;
 using System.Windows.Controls;
-using FactoryMethod.Creators;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace FactoryMethod
+namespace L_1
 {
-  
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-     
-        private void ColorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (ColorComboBox.SelectedItem is ComboBoxItem selectedItem)
-            {
-                // Remove all previously created shapes
-                FiguresPanel.Children.Clear();
-
-                // Determine which set of creators to use based on the selected colour
-                CircleCreator circleCreator;
-                SquareCreator squareCreator;
-                TriangleCreator triangleCreator;
-
-                switch (selectedItem.Content.ToString())
-                {
-                    case "Красный":
-                        circleCreator = new RedCircleCreator();
-                        squareCreator = new RedSquareCreator();
-                        triangleCreator = new RedTriangleCreator();
-                        break;
-                    case "Синий":
-                        circleCreator = new BlueCircleCreator();
-                        squareCreator = new BlueSquareCreator();
-                        triangleCreator = new BlueTriangleCreator();
-                        break;
-                    case "Зелёный":
-                        circleCreator = new GreenCircleCreator();
-                        squareCreator = new GreenSquareCreator();
-                        triangleCreator = new GreenTriangleCreator();
-                        break;
-                    default:
-                        return;
-                }
-
-                // Create the figures and add their UI elements to the panel
-                var circle = circleCreator.CreateCircle();
-                FiguresPanel.Children.Add(circle.CreateUIElement());
-
-                var square = squareCreator.CreateSquare();
-                FiguresPanel.Children.Add(square.CreateUIElement());
-
-                var triangle = triangleCreator.CreateTriangle();
-                FiguresPanel.Children.Add(triangle.CreateUIElement());
-            }
         }
     }
 }
